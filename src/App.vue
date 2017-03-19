5f0477a94380573ec22193a8103f0a9bd8d1f828
Margaret2 <mlh.hutchins@gmail.com>
2017-03-18 21:49:15 -0500

    add mj-image

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-      <mj-button href="google.com">Click for google</mj-button>
+      <mj-button href="google.com">Click me</mj-button>
+      <mj-image src="https://unsplash.com/collections/184253/winter?photo=vwRomKDKuRU"></mj-image>
   </div>
 </template>
 
 <script>
 import mjButton from './mj-button.vue'
+import mjImage from './mj-image.vue'
 export default {
-  components: { mjButton }
+  components: { mjButton, mjImage }
 }
 </script>
 
---
+++ src/mj-image.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+  <table :align="align || 'center'" :border="border || 0" cellpadding="0" cellspacing="0" role="presentation" :style="[styles.table, customStyles]">
+    <tbody>
+      <tr>
+        <td :style="[styles.td, customStyles]">
+          <a v-if="href" :href="href || ''" :target="target || '_blank'">
+            <img :alt="alt || ''" :src="src || ''" :height="height || 'auto'" :style="[styles.img, customStyles]" :title="title || ''" :width="width || ''">
+          </a>
+          <img :alt="alt || ''" :src="src || ''" :height="height || 'auto'" :style="[styles.img, customStyles]" :title="title || ''" :width="width || ''">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ['align', 'alt', 'border', 'border-radius','custom-styles', 'height', 'href', 'padding-arr','src', 'target', 'title', 'width'],
+  data: function(){
+    return {
+      styles: {
+        table: {
+          borderCollapse: 'collapse',
+          borderSpacing: '0px'
+        },
+        td: {
+          width: this.width + 'px' || ''
+        },
+        img: {
+          border: 'none',
+          borderRadius: '',
+          height: 'auto',
+          display: 'block',
+          outline: 'none',
+          textDecoration: 'none',
+          width: '100%'
+        }
+      }
+    }
+  },
+  computed: {
+    getCoumputedWidth: function(){
+        // return width TODO
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
+<!--
+            <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="center" border="0">
+              <tbody>
+                  <tr>
+                    <td style="width:100px;">
+                      <img alt="" title="" height="auto" src="/assets/img/logo-small.png"
+                      style="border:none;border-radius:;display:block;outline:none;text-decoration:none;width:100%;height:auto;"
+                      width="100">
+                  </td>
+                      </tr>
+               </tbody>
+             </table>
+-->
