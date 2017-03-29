24259a757726d622c1024921ff253c633c639e7c
Margaret2 <mlh.hutchins@gmail.com>
2017-03-28 21:25:24 -0500

    add mj-section

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <!-- testing all the components -->
 <template>
     <div :style="styles">
+      <mj-section full-width="true" background-url="https://images.unsplash.com/photo-1487564149407-9fb3522e2ceb?dpr=1&auto=compress,format&fit=crop&w=600&h=200&q=80&cs=tinysrgb&crop=">
+        <h1 style="font-size: 20px;">sljs sl lkjs </h1>
+      </mj-section>
       <mj-button href="google.com">Click me</mj-button>
       <!--temporary table so the divider will work-->
       <table><tbody>
@@ -15,10 +18,11 @@
 import mjButton from './mj-button.vue'
 import mjDivider from './mj-divider.vue'
 import mjImage from './mj-image.vue'
+import mjSection from './mj-section.vue'
 import mjText from './mj-text.vue'
 export default {
   props: ['background-color', 'width'],
-  components: {mjButton, mjDivider, mjImage, mjText },
+  components: {mjButton, mjDivider, mjImage, mjSection, mjText },
   data: function(){
     return {
       styles: {
---
+++ src/mj-section.vue
@@ -0,0 +1,68 @@
+<!--TODO: get width --> 
+<template lang="html">
+  <!--[if mso | IE]>
+  <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+    <tr>
+      <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+  <![endif]-->
+<div v-if="backgroundUrl" :style="divStyle">
+  <!--[if mso | IE]>
+    <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="GETWIDTH">
+        <v:fill origin="0.5, 0" position="0.5,0" type="tile" :src="backgroundUrl" />
+        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
+    <![endif]-->
+  <table role="presentation" cellpadding="0" cellspacing="0" :style="tableStyle" align="center" border="0">
+    <tbody>
+      <tr>
+        <td :style="tdStyle">
+          <slot></slot>
+        </td>
+      </tr>
+</tbody>
+</table>
+<!--[if mso | IE]>
+  </v:textbox>
+</v:rect>
+<![endif]-->
+</div>
+<div v-else :style="divStyle">
+  <table role="presentation" cellpadding="0" cellspacing="0" :style="tableStyle" align="center" border="0">
+    <tbody>
+      <tr>
+        <td :style="tdStyle">
+          <slot></slot>
+        </td>
+      </tr>
+</tbody>
+</table>
+</div>
+<!--[if mso | IE]>
+</td></tr></table>
+<![endif]-->
+</template>
+
+<script>
+export default {
+  props:['background-url', 'full-width'],
+  data: function(){
+    return {
+      divStyle: {
+        margin: '0px auto',
+        maxWidth: this.fullWidth ? '' : '600px',
+        background:  `url(${this.backgroundUrl}) top center / auto repeat`,
+        width: this.fullWidth ? '100%' : ''
+      },
+      tableStyle: {
+        fontSize: '0px',
+        width: '100%'
+      },
+      tdStyle: {
+        textAlign: 'center',
+        verticalAlign: 'top',
+        fontSize: '0px',
+        padding: '0px 20px'
+      }
+    }
+  }
+}
+</script>
